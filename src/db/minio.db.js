5d6487ada97edee9689cc9c892f1d3d674a24e36
lengleng9090 <lengleng9090@gmail.com>
2022-07-16 14:08:30 +0700

    [FIX] unused file and fix minio config

--- src/db/minio.db.js
+++ src/db/minio.db.js
@@ -1,9 +1,9 @@
-const Minio = require("minio");
+const Minio = require('minio');
 
 const minioClient = new Minio.Client({
   endPoint: process.env.MINIO_URL,
   port: parseInt(process.env.MINIO_PORT),
-  useSSL: false,
+  useSSL: true,
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
 });
